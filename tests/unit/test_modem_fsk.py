c1543e0760198f5c66d76d0cfb2153a3608d95ea
JayPay108 <jaypay0412@gmail.com>
2025-05-31 20:56:59 +0000

    added mod/demod logic to fsk modem

--- tests/unit/test_modem_fsk.py
+++ tests/unit/test_modem_fsk.py
@@ -5,19 +5,29 @@ from sigkit.metrics.integrity import calculate_ber
 from sigkit.modem.fsk import FSK
 
 
-@pytest.mark.parametrize("n_components", [2, 4, 8, 16, 32, 64])
+@pytest.mark.parametrize("n_components", [2, 4, 8, 16, 32])
 @pytest.mark.parametrize("cf", [0.0, 256.0])
 def test_fsk_modem(n_components, cf):
     """
     For each supported M-FSK (M=2,4,8,16,32,64) and for CF=0 (baseband) and CF=256 Hz,
     modulate random bits, demodulate them, and assert BER==0.
     """
+    sample_rate = 1024
+    symbol_rate = 32
 
     modem = FSK(
+        sample_rate=sample_rate,
+        symbol_rate=symbol_rate,
+        n_components=n_components,
+        cf=cf,
     )
 
-    bits = np.zeros(10, dtype=np.complex64)
-    demod_bits = modem.demodulate(bits)
+    num_symbols = 100
+    bits = np.random.randint(
+        0, 2, size=num_symbols * modem.bits_per_symbol, dtype=np.uint8
+    )
+    signal = modem.modulate(bits)
+    demod_bits = modem.demodulate(signal)
 
     assert demod_bits.shape == bits.shape
     assert np.array_equal(bits, demod_bits)
